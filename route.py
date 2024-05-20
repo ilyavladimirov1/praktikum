44ea30594f4d2d4558261b4b5b5b6b4370e8d541
ilyavladimirov1 <ilya_vladimirov_2004@list.ru>
2024-05-20 21:46:30 +0300

    update route

--- route.py
+++ route.py
@@ -10,25 +10,30 @@ router = APIRouter()
 
 
 @router.get("/film", response_model=List[schemas.Film])
-def get_films(year: int = None, tags: List[str] = [], db: Session = Depends(SessionLocal)):
+def get_films(year: int = None, tags: List[str] = []):
+    db: Session = Depends(SessionLocal)
     return crud.get_films(db, year, tags)
 
 
 @router.get("/film/{film_id}", response_model=schemas.Film)
-def get_film(film_id: int, db: Session = Depends(SessionLocal)):
+def get_film(film_id: int):
+    db: Session = Depends(SessionLocal)
     return crud.get_film(db, film_id)
 
 
 @router.post("/film/", response_model=schemas.Film)
-def create_film(film: schemas.FilmCreate, db: Session = Depends(SessionLocal)):
+def create_film(film: schemas.FilmCreate):
+    db: Session = Depends(SessionLocal)
     return crud.create_film(db, film)
 
 
 @router.put("/film/{film_id}", response_model=schemas.Film)
-def update_film(film_id: int, film: schemas.FilmCreate, db: Session = Depends(SessionLocal)):
+def update_film(film_id: int, film: schemas.FilmCreate):
+    db: Session = Depends(SessionLocal)
     return crud.update_film(db, film_id, film)
 
 
 @router.delete("/film/{film_id}", response_model=schemas.Film)
-def delete_film(film_id: int, db: Session = Depends(SessionLocal)):
+def delete_film(film_id: int):
+    db: Session = Depends(SessionLocal)
     return crud.delete_film(db, film_id)
